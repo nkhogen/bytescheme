a6835dbbfbd0f77226aa279cfa517cff279480eb
Naorem Khogendro Singh <nkhogen@Naorems-MacBook-Pro.local>
2016-12-14 11:14:09 -0800

    Info and other stuffs

---
+++ services/controlboard/private/bin/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo systemctl stop controlboard.service
+
+sudo /home/pi/.acme.sh/acme.sh --issue --standalone -d video1.bytescheme.com --certpath /controlboard/conf/security/ssl/bytescheme.cer --keypath /controlboard/conf/security/ssl/bytescheme-private.key --capath /controlboard/conf/security/ssl/ca.cer --fullchainpath /controlboard/conf/security/ssl/fullchain.cer
+
+sudo openssl pkcs12 -export -in /controlboard/conf/security/ssl/bytescheme.cer -inkey /controlboard/conf/security/ssl/bytescheme-private.key  -out /controlboard/conf/security/ssl/bytescheme.p12
+
+sudo systemctl start controlboard.service
+
+# To generate self-signed (Optional)
+keytool -genkey -alias jetty -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650
+
+server.ssl.key-store: bytescheme.p12
+server.ssl.key-store-password: mypassword
+server.ssl.keyStoreType: PKCS12
+server.ssl.keyAlias: jetty
--- services/controlboard/public/bin/generate.sh
+++ services/controlboard/public/bin/generate.sh
@@ -5,15 +5,16 @@ sudo /home/ubuntu/.acme.sh/acme.sh --issue --standalone -d bytescheme.com -d www
 
 sudo openssl pkcs12 -export -in /controlboard/conf/security/ssl/bytescheme.cer -inkey /controlboard/conf/security/ssl/bytescheme.key  -out /controlboard/conf/security/ssl/bytescheme.p12
 
-sudo keytool -importkeystore -srckeystore /controlboard/conf/security/ssl/bytescheme.p12 -srcstoretype PKCS12 -destkeystore /controlboard/conf/security/ssl/keystore.jks -deststoretype JKS
+# If we want to use JKS (Optional)
+ sudo keytool -importkeystore -srckeystore /controlboard/conf/security/ssl/bytescheme.p12 -srcstoretype PKCS12 -destkeystore /controlboard/conf/security/ssl/keystore.jks -deststoretype JKS
 
 sudo systemctl start controlboard.service
 
 
-or use PKCS12
-
+# To generate self-signed (Optional)
 keytool -genkey -alias jetty -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650
-server.ssl.key-store: keystore.p12
+
+server.ssl.key-store: bytescheme.p12
 server.ssl.key-store-password: mypassword
 server.ssl.keyStoreType: PKCS12
 server.ssl.keyAlias: jetty
