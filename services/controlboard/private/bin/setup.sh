68476cc7f317f3cacdc5baad5c060f2e9e2a6047
Naorem Khogendro Singh <nkhogen@Naorems-MacBook-Pro.local>
2017-11-25 20:24:39 -0800

    Proto module added

--- services/controlboard/private/bin/setup.sh
+++ services/controlboard/private/bin/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo mkdir -p /controlboard/conf
+sudo mkdir -p /controlboard/conf/security/ssl
 sudo mkdir -p /controlboard/libs
 sudo mkdir -p /controlboard/logs
 sudo mkdir -p /controlboard/run
 sudo mkdir -p /controlboard/bin
+
+# Access video device /dev/video0
+sudo modprobe bcm2835-v4l2
